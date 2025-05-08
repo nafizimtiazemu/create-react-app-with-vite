c0e45d1382d06962f38e8caeb4ccbce4a7c3cb7d
Nafiz Imtiaz <nafizimtiaz@Nafizs-MacBook-Air.local>
2025-05-07 22:38:44 -0400

    URL of Guessing Game is added

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,8 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+        <a href="https://ttpr-guessing-game-olive.vercel.app" target="_blank">
+          <img src={GuessingGame} className="logo" alt="Game logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />

479bde2a21d8289e236848ddc091020704c45321
Nafiz Imtiaz <nafizimtiaz@Nafizs-MacBook-Air.local>
2025-05-07 23:46:03 -0400

    Added function to fetch cat fact

--- src/CatFact.jsx
+++ src/CatFact.jsx
@@ -1,2 +1,15 @@
 import { useEffect, useState } from 'react';
 
+function CatFact() {
+  const [fact, setFact] = useState('');
+
+  useEffect(() => {
+    fetch('https://catfact.ninja/fact')
+      .then(res => res.json())
+      .then(data => setFact(data.fact));
+  }, []);
+
+  return <p>{fact}</p>;
+}
+
+export default CatFact;
